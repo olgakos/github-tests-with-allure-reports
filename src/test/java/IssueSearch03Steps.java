33cf9db83ccc3265467c026a218cc6727884bb25
Olga <49266934+olgakos@users.noreply.github.com>
2022-02-26 17:27:32 +0300

    Step02: add Lambda Steps in Allure-report to PManager :)

---
+++ src/test/java/IssueSearch03Steps.java
@@ -0,0 +1,44 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class IssueSearch03Steps {
+
+    @BeforeEach
+    void preconditionBrowser() {
+        browserSize = "1920x1080";
+    }
+    @AfterEach
+    void closeBrowser() {
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    void issueSearchTest02AddAllure() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открыть главную страницу GitHub", () -> {
+            open("https://github.com");
+        });
+        step("Заполнить поле Поиск данными", () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys("olgakos/github-tests-with-allure-reports");
+        });
+        step("Нажать кнопку Поиск", () -> {
+            $(".header-search-input").submit();
+        });
+        step("Проверить, что на открывшейся стр. есть кнопка Issues", () -> {
+            $(By.partialLinkText("Issues")).should(Condition.exist);
+        });
+    }
+}
